813eac526ee2163b09e1a90578b3f222cf7d78aa
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2019-12-15 23:48:21 -0600

    added count and countButton components, styling, and updated app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images.png';
 import './App.css';
+import CountButton from './CountButton.js';
+import Count from './Count.js';
+import fizzbuzz from './fizzbuzz.js'
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        showComponent: false,
+        numbers: ['initial']
+      }
+    this.handleClick = this.handleClick.bind(this);
+    }
+  handleClick = () => {
+    console.log('clicked');
+    this.setState({
+      showComponent: true,
+      numbers: fizzbuzz()
+    })
+  };
+
+  render () {
+    return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>FizzBuzz!</h1>
+        <div>
+          {this.state.showComponent ? <Count numbers={this.state.numbers}/> : null} 
+        </div>
+        <h3>A classic programming interview question</h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Write a program that prints the numbers from 1 to 100. If it’s a multiple of 3, it should print “Fizz”. If it’s a multiple of 5, it should print “Buzz”. If it’s a multiple of 3 and 5, it should print “Fizz Buzz”.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <CountButton addCount={this.handleClick} />
+        </div>
       </header>
     </div>
-  );
+    );
+  }
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import fizzbuzz from './fizzbuzz';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+const result = fizzbuzz();
+
+test('should log "Fizz" for multiples of 3', function() {
+      expect(result[2]).toBe("Fizz");
+});
+test('should log "Buzz" for multiples of 5', function() {
+      expect(result[4]).toBe("Buzz");
+});
+test('should log "FizzBuzz" for multiples of 15', function() {
+      expect(result[14]).toBe("FizzBuzz");
+});
+test('should log a number for non multiples of 3 or 5', function() {
+      expect(typeof result[3]).toBe("number");
+});
+test('should return an array', () => {
+      expect(Array.isArray(result)).toBeTruthy();
+})
+test('should return correct number of responsees', () => {
+      expect(result.length).toBe(100);
+})
+
