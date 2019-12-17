3af824d7c78948bf39658b969cb80aedd714f09b
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2019-12-17 16:12:07 -0600

    attempt to fix git setup error by pushing up earlier commit

---
+++ client/build/precache-manifest.9a8b1a7bb2d97fc99be70f7305b29983.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e360ed92962a404c389699c0fa81d8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3285927ec0437d96b08a",
+    "url": "/static/css/main.b9fcebb5.chunk.css"
+  },
+  {
+    "revision": "d99d233f72373ac8ff3e",
+    "url": "/static/js/2.d72e60cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d72e60cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3285927ec0437d96b08a",
+    "url": "/static/js/main.07b1f6a7.chunk.js"
+  },
+  {
+    "revision": "fa5f335911943594e4c1",
+    "url": "/static/js/runtime-main.863622fb.js"
+  },
+  {
+    "revision": "f575917de0bb75dbd3645eac9762fa3d",
+    "url": "/static/media/images.f575917d.png"
+  }
+]);
