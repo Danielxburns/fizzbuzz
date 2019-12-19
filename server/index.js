a5b14efe855a6dcde183edab212908f0c2a731e1
Danielxburns <41316262+Danielxburns@users.noreply.github.com>
2019-12-18 23:37:48 -0600

    Docker images running in containers

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
 app.use(express.static(__dirname + '/../client/build'));
 
-app.listen(8080, () => console.log('MyFizzbuzz listening on port 8080!'));
+app.get('/', (req, res) => { 
+    res.send(
+      `Yeah, we're serving on up!`);
+  });
+
+app.listen(8080, () => console.log('MyFizzbuzz server listening on port 8080!'));
